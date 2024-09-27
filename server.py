cc022d422fabf85f6c4014930ef84f1f243ca227
edutra <ebodutra@gmail.com>
2024-09-27 02:30:00 -0300

    made changes to server file

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 from games.keno import Keno
+import db
+
 app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/users/{username}")
+async def get_user(username: str):
+    return db.get_user(username)
 
 if __name__ == "__main__":
+
     app.include_router(Keno.router)
     uvicorn.run(app, host="0.0.0.0", port=8000)
